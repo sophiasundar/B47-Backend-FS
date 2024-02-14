04cf8eb30e86f09d893f23bbf7415bff12b1923c
sophiasundar <sophiagifta17@gmail.com>
2024-02-14 16:52:06 +0530

    commit

--- routes/routeUser.js
+++ routes/routeUser.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { genPassword, createUser, getUserByName } from "../helper/helperUser.js";
-
-
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 const router = express.Router();
 
 
@@ -36,14 +36,27 @@ router.post('/login',async(req,res)=>{
   const {username,password} = req.body;
   console.log(username,password)
    // validate if username is present
-     const userDataDB = await getUserByName(username);
-       console.log(userDataDB);
+     const userFromDB = await getUserByName(username);
+       console.log(userFromDB);
 
-       if(!userDataDB){
+       if(!userFromDB){
         res.status(400).send({message: " Invalid Credentials "})
         return;
     }
-  res.send(userDataDB); 
+
+    const passwordDb = userFromDB.password;
+      const isPasswordMatch = await bcrypt.compare(password,passwordDb)
+      
+      if(!isPasswordMatch){
+        res.status(400).send({message: " Invalid Credentials "})
+        return;
+    }
+
+    // generate token
+         const token = jwt.sign({id:userFromDB._id},process.env.SECRET_KEY);
+           
+       
+         res.send(isPasswordMatch); 
 });
 
 
