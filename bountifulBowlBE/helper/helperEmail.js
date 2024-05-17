2e430a5db6460d3d9ff325791c91287d02592f8a
sophiasundar <sophiagifta17@gmail.com>
2024-05-17 10:44:01 +0530

    commit

--- bountifulBowlBE/helper/helperEmail.js
+++
@@ -1,9 +0,0 @@
-// import { ObjectId } from "mongodb";
-import { client } from "../index.js";
-
-// export async function addEmail(newemail){
-//     return await client
-//     .db("bountiful")
-//     .collection("email")
-//     .insertOne(newemail)
-// }
