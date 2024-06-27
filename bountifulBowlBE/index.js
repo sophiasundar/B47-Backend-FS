c7541e7e5c8020dda78693e3561eee4086f86121
sophiasundar <sophiagifta17@gmail.com>
2024-06-27 11:27:56 +0530

    commit

--- bountifulBowlBE/index.js
+++ bountifulBowlBE/index.js
@@ -7,7 +7,7 @@ import { foodRouter } from "./routes/routeFood.js";
 import { OrphManagerRouter } from "./routes/routeOrphanInfo.js";
 import { emailRouter } from "./routes/routeEmail.js";
 import nodemailer from 'nodemailer';
-import { ObjectId } from "mongodb";
+
 
 
 const app = express();
@@ -29,77 +29,75 @@ const MONGO_URL = process.env.MONGO_URL;
  export const client = await createConnection()
 
 
-//  const dbName = 'bountiful';
-export async function sendEmail(name, orphanagename, email, address, banquetname, banmanagername, banquetemail){
-  const client = new MongoClient(MONGO_URL);
-  
-  try{
-           await client.connect();
-         //   const db = client.db(dbName);
-         //   console.log(dbName);
-        
-
-      //   const banDetailsCollect = db.collection('food');
+//   const dbName = 'bountiful';
+  async function sendEmail(name, orphanagename, email, address, banquetname, banmanagername, banquetemails){
+    
+      const client = new MongoClient(MONGO_URL);
+      await client.connect();
    
-             
-      //   const banquetDetails = await banDetailsCollect.findOne({ _id: new ObjectId(banquetDetailsId)})
-      //   console.log(typeof(banquetDetailsId))
-      //   console.log(banquetDetailsId)
-      //     if (!banquetDetails){
-      //       throw new Error('Banquet details not found');
-      //     }
-             
-       
-        // email service
-        const transporter = nodemailer.createTransport({
-          service:"gmail",
-           auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASSWORD
-           }
-          });
+      // const db = client.db(dbName);
+      // const banDetailsCollect = db.collection('food');
 
-          const mailOptions = {
-             from: `${email}`,
-             to: `${banquetemail}`,
-             subject: 'Orphanage Booking Confirmation',
-             html: `<h1>Food Takein Confirmation by Orphanage Management</h1>
+      // const banquetDetails = await banDetailsCollect.findOne({ _id: new ObjectId(banquetDetailsId)})
+      // console.log(typeof(banquetDetailsId))
+      // console.log(banquetDetailsId)
+      //   if (!banquetDetails){
+      //     throw new Error('Banquet details not found');
+      //   }
 
-             <p>Dear ${banmanagername}, </p>
-             <p>This email confirms that ${name} orphanage has agreed to Takein the food.</p>
+         const transporter = nodemailer.createTransport({
+            service:"gmail",
+             auth: {
+              user: process.env.EMAIL_USER,
+              pass: process.env.EMAIL_PASSWORD
+             }
+            });
       
-             <p><b>Orphanage Details: </b></p>
-             
-             <ul>
-              <li>Name: ${name}</li>
-              <li>Orphanage Name: ${orphanagename}</li>
-              <li>Address: ${address}</li>
-              <li>email: ${email} </li>
-             </ul>
       
-             <p><b>Banquet Details:</b></p>
-             <ul>
-              <li>Name: ${banmanagername} </li>
-              <li>Banquet Hall: ${banquetname}</li>
-              <li>Banquet Manager Email: ${banquetemail}</li>
-             </ul>
-             <p><b>Thanks and Regards, </b></p>
-                   <p>${name}<p/>
-                <p>${orphanagename}</p>
+         const mailOptions = {
+         from: `${email}`,
+         to: `${banquetemails}`,
+         subject: 'Orphanage Booking Confirmation',
+         html: `<h1>Food Takein Confirmation by Orphanage Management</h1>
 
-             `
-          }
+         <p>Dear ${banmanagername}, </p>
+         <p>This email confirms that ${name} orphanage has agreed to Takein the food.</p>
+  
+         <p><b>Orphanage Details: </b></p>
+         
+         <ul>
+          <li>Name: ${name}</li>
+          <li>Orphanage Name: ${orphanagename}</li>
+          <li>Address: ${address}</li>
+          <li>email: ${email} </li>
+         </ul>
+  
+         <p><b>Banquet Details:</b></p>
+         <ul>
+          <li>Name: ${banmanagername} </li>
+          <li>Banquet Hall: ${banquetname}</li>
+          <li>Banquet Manager Email: ${banquetemails}</li>
+         </ul>
+         <p><b>Thanks and Regards, </b></p>
+               <p>${name}<p/>
+            <p>${orphanagename}</p>
 
-          await transporter.sendMail(mailOptions);
-          console.log('Email sent successfully!')
-    }catch (error){
-      console.error('Error fetching data or sending email:',error);
-    }finally{
-        await client.close();
-    }
+         `
+      };
 
+      transporter.sendMail(mailOptions, function(error){
+         if(error){
+            console.log(error)
+           
+         }else{
+            console.log("Email sent successfully")
+           
+         }
+      });
 
-  };
+   
+
+};
 
    // nodemail
 
@@ -107,7 +105,23 @@ export async function sendEmail(name, orphanagename, email, address, banquetname
          res.send('Hey! Hi, 🙋‍♀️👋🙌🏽🙏🏽');
       });
 
-     //user
+     // email
+       app.post('/sendemail', async(req,res)=>{
+
+        try{     
+         const { name, orphanagename, email, address, banquetname, banmanagername, banquetemails } = req.body.newEmail;
+         console.log(name, orphanagename, email, address, banquetname, banmanagername, banquetemails);
+         await sendEmail( name, orphanagename, email, address, banquetname, banmanagername, banquetemails);
+         res.json({ message: 'Email sent successfully! '});
+      }catch (error){
+          console.log(error);
+          res.status(500).json({ message: "Error sending email" })
+      }
+      
+         
+       });
+     
+       //user
      app.use("/users", userRouter);
 
      //foodlist
@@ -116,9 +130,8 @@ export async function sendEmail(name, orphanagename, email, address, banquetname
      //Orph Info
      app.use('/orphinfo',OrphManagerRouter);
      
-     //email
-     app.use('/sendemail',emailRouter);  
-
+   //   email banquet
+     app.use('/banquet', emailRouter)
 
  app.listen(PORT, ()=> 
     console.log("Server started on the PORT", PORT)
--- bountifulBowlBE/routes/routeEmail.js
+++ bountifulBowlBE/routes/routeEmail.js
@@ -1,28 +1,22 @@
-import { sendEmail } from '../index.js';
-import express from 'express';
-// import { verifyToken } from "../middleware/auth.js";  
+
+import express from "express";
+import { getBanquetEmail } from '../helper/helperEmail.js';
+
+
 const router = express.Router();
 
-
-
-    
-   // verifyToken(['orphanage-manager']),
-router.post("/",  async (req,res)=>{
-
-   const { name, orphanagename, email, address, banquetname, banmanagername, banquetemail } = req.body;
-
-   try{
-      await sendEmail( name, orphanagename, email, address, banquetname, banmanagername, banquetemail );
-      res.json({ message: 'Email sent successfully! '});
-   }catch (error){
-       console.log(error);
-       res.status(500).json({ message: "Error sending email" })
-   }
+router.get("/", async (req,res)=>{
    
+   try{
+      const emails = await getBanquetEmail();
+      res.json(emails); 
+   }catch{
+      console.error('Error retrieving emails:', error);
+    res.status(500).json({ message: 'Error retrieving emails' });
+   }
 });
-
-
-
-
-
+   
 export const emailRouter = router;
+      
+
+      
