4c360cc90cb5773f3f4831e0f9b6b01a2bb96eb4
sophiasundar <sophiagifta17@gmail.com>
2024-07-01 17:07:36 +0530

    commit

--- bountifulBowlBE/index.js
+++ bountifulBowlBE/index.js
@@ -43,7 +43,8 @@ const MONGO_URL = process.env.MONGO_URL;
              }
             });
       
-      
+      console.log(process.env.EMAIL_USER, typeof(process.env.EMAIL_USER));
+      console.log(process.env.EMAIL_PASSWORD, typeof(process.env.EMAIL_PASSWORD));
          const mailOptions = {
          from: `${email}`,
          to: `${banquetemails}`,
